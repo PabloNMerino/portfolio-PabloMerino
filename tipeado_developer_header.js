85a606b2379c2088d6b78bb4b84017bf2103acaa
PabloNMerino <pablonicolasm@hotmail.com>
2023-01-05 16:23:48 -0300

    correcion minima

--- tipeado_developer_header.js
+++ tipeado_developer_header.js
@@ -1,3 +1,14 @@
+/*const typed = new Typed('.typed', {
+    strings: ['developer'],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+    cursorChar: '|',
+    showCursor: true
+})
+*/
+
 document.querySelectorAll('.typed').forEach(function(el) {
     new Typed(el, {
         strings: ['developer'],
